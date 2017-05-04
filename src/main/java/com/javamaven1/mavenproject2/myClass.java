c0f6790b8e53ae80a79dcb371738a703bd45990d
usman.javaid1 <usman.javaid1@NISB-GSS-P4057.telenor.com.pk>
2017-05-04 15:29:35 +0500

    Commit 1

---
+++ src/main/java/com/javamaven1/mavenproject2/myClass.java
@@ -0,0 +1,9 @@
+package com.javamaven1.mavenproject2;
+import com.javamaven1.ujmavenproject1.NewClass;
+
+public class myClass {
+    
+    public static void main(String[] args){
+    com.javamaven1.ujmavenproject1.NewClass.printsomething();
+    }
+}
